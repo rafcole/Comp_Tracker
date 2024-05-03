8eb893c22182a15f4d9153d6fd569e746615dc33
rafcole <39354464+rafcole@users.noreply.github.com>
2024-05-02 19:03:04 -0700

    api login test route added

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+import json
+from scraper import login
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Wordddld!</p>"
+    return render_template("index.html")
+
+@app.route("/login", methods=['POST'])
+def idk():
+    login("rio-salado-black-rifle-may-2024-2")
+    return 'login'
